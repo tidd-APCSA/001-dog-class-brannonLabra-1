989fd531630ce4e78dca5a58667d296b365dabc5
Brannon Labra <brannonalabra@gmail.com>
2020-10-15 14:50:03 +0000

    Added getter methods, added constructor, output all characteristics of baxter.

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
   public static void main(String[] args) {
     // test your code here by creating a three different dogs from the Dog class. Be sure you give each dog different characteristics. 
+  Dog baxter = new Dog("Baxter	🐶", "Poodle", 20, "brown", true);
+  Dog Leila = new Dog("Leila", "Chiwaua", 12.5, "White", false);
+  Dog silas = new Dog("Silas", "Golden Doodle", 30, "Tan", true);
+
+  System.out.println(baxter.getName());
+  System.out.println(baxter.getBreed());
+  System.out.println(baxter.getWeight());
+  System.out.println(baxter.getColor());
+  System.out.println(baxter.getHouseTrained());
+
+
+
+
   }
 }
